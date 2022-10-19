22837c77fc912e651850ac974ece75bfd989d8c2
Ebenezhar <ebenezharselvakumar@gmail.com>
2022-10-19 23:01:24 +0530

    resume updated

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
       <h2 className='header3 p-5 mr-5 ml-5 mb-5'> Resume</h2>
       <div className="d-flex justify-content-center">
         <button className='p-3 mb-5 bg-white rounded hover-shadow' >
-          <a className='col-lg-12 display-5 justify-content-center text-decoration-none text-dark font-weight-bold' target="_blank" href='https://drive.google.com/file/d/19Ltd9qwu4S4gXdRBUgw88oV0YUlkrNa-/view?usp=sharing'>Click here to view Resume</a>
+          <a className='col-lg-12 display-5 justify-content-center text-decoration-none text-dark font-weight-bold' target="_blank" href='https://drive.google.com/file/d/1ryMIh2UYbWBGU6bykG2atTozGAtqerSM/view?usp=sharing'>Click here to view Resume</a>
         </button>
       </div>
     </div>
